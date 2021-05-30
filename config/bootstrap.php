9b874d18d2b404c9dc884cede921e7164b3fd20e
Jascha030 <contact@jaschavanaalst.nl>
2021-05-30 22:06:03 +0200

    Bootstrap and config files

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,1 +1,17 @@
 <?php
+
+use DI\ContainerBuilder;
+
+require (require dirname(__FILE__, 2) . 'includes/loader.php') (__FILE__);
+
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->addDefinitions(__DIR__ . '/containerConfig.php');
+
+try {
+    $container = $containerBuilder->build();
+} catch (Exception $e) {
+    (new Symfony\Component\Console\Output\ConsoleOutput())->writeln($e->getMessage());
+    exit(1);
+}
+
+return $container;
