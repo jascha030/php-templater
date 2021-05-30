95130e0032e25ea4c458eab9b87368e5cffff5fb
Jascha030 <contact@jaschavanaalst.nl>
2021-05-30 22:04:49 +0200

    Added Exceptions

--- src/Exception/OutputFileExistsException.php
+++ src/Exception/OutputFileExistsException.php
@@ -2,7 +2,21 @@
 
 namespace Jascha030\PTempo\Exception;
 
-class OutputFileExistsException
-{
+use Jascha030\PTempo\Templater\Templater;
 
+/**
+ * Class OutputFileExistsException
+ * @package Jascha030\PTempo\Exception
+ */
+class OutputFileExistsException extends TemplateFileExceptionAbstract
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function getTemplateString(): string
+    {
+        $class = Templater::class;
+
+        return 'Output file: "%s" already exists. If you want to overwrite the outputFile, enable "$overwrite" in ' . $class . '::renderTemplate()';
+    }
 }
