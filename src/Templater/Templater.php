7c137c787163d1e2a9fe806fd77be462b1cccff0
Jascha030 <contact@jaschavanaalst.nl>
2021-05-30 04:35:56 +0200

    Started templater

--- src/Templater/Templater.php
+++ src/Templater/Templater.php
@@ -2,7 +2,52 @@
 
 namespace Jascha030\PTempo\Templater;
 
-class Templater
-{
+use Jascha030\PTempo\Engine\StandardTemplateEngine;
+use Jascha030\PTempo\Engine\TwigTemplateEngine;
+use Symfony\Component\Filesystem\Filesystem;
 
+/**
+ * Class Templater
+ * @package Jascha030\PTempo\Templater
+ */
+final class Templater
+{
+    /**
+     * Available implementations of TemplateEngineInterface
+     */
+    public const TEMPLATE_ENGINES = [
+        StandardTemplateEngine::class,
+        TwigTemplateEngine::class
+    ];
+
+    private Filesystem $fileSystem;
+
+    private StandardTemplateEngine $templateEngine;
+
+    public function __construct(Filesystem $fileSystem)
+    {
+        $this->fileSystem = $fileSystem;
+    }
+
+    public function setEngine(StandardTemplateEngine $engine): self
+    {
+        $this->templateEngine = $engine;
+
+        return $this;
+    }
+
+    public function renderTemplate(string $templatePath, array $userInput, ?string $outputPath = null): void
+    {
+        if (! $this->fileSystem->exists($templatePath)) {
+            // todo: throw custom exception
+        }
+
+        $template = $this->templateEngine->boilerplate($templatePath, $userInput);
+
+        if (! $outputPath) {
+            $outputPath = $templatePath;
+        }
+
+        $this->fileSystem->dumpFile($outputPath, $template);
+    }
 }
