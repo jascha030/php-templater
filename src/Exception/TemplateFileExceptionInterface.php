6b70e9b572bed3f13f0292a1f36b59fbc0798061
Jascha030 <contact@jaschavanaalst.nl>
2021-05-30 22:04:27 +0200

    TemplateFileExceptionInterface

--- src/Exception/TemplateFileExceptionInterface.php
+++ src/Exception/TemplateFileExceptionInterface.php
@@ -2,7 +2,32 @@
 
 namespace Jascha030\PTempo\Exception;
 
-class TemplateExceptionInterface
+/**
+ * Interface TemplateFileExceptionInterface
+ * @package Jascha030\PTempo\Exception
+ */
+interface TemplateFileExceptionInterface
 {
+    /**
+     * Provide a path string to be injected in the template string
+     *
+     * @param string $fileName
+     */
+    public function __construct(string $fileName);
 
+    /**
+     * Provide template string to be injected with $filePath.
+     *
+     * @return string
+     */
+    public static function getTemplateString(): string;
+
+    /**
+     * Creates the message, to be passed to \Exception constructor
+     *
+     * @param string $filePath
+     *
+     * @return string
+     */
+    public static function createMessage(string $filePath): string;
 }
